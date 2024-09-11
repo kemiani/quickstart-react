f00cc6d7060918bab22e4d684e076104c393a098
null <null>
2024-09-11 14:34:01 -0300

    valen

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import Button from "./components/base/Button";
 import Vapi from "@vapi-ai/web";
 import { isPublicKeyMissingError } from "./utils";
 
-// Put your Vapi Public Key below.
-const vapi = new Vapi("0000XXXX-XXXX-XXXX-XXXX-XXXXXXXX0000");
+// Clave pública de Vapi
+const vapi = new Vapi("fb11f167-3d2b-443e-b6e6-22e944daa421");
 
 const App = () => {
   const [connecting, setConnecting] = useState(false);
   const [connected, setConnected] = useState(false);
-
   const [assistantIsSpeaking, setAssistantIsSpeaking] = useState(false);
   const [volumeLevel, setVolumeLevel] = useState(0);
 
@@ -22,14 +21,12 @@ const App = () => {
     vapi.on("call-start", () => {
       setConnecting(false);
       setConnected(true);
-
       setShowPublicKeyInvalidMessage(false);
     });
 
     vapi.on("call-end", () => {
       setConnecting(false);
       setConnected(false);
-
       setShowPublicKeyInvalidMessage(false);
     });
 
@@ -47,7 +44,6 @@ const App = () => {
 
     vapi.on("error", (error) => {
       console.error(error);
-
       setConnecting(false);
       if (isPublicKeyMissingError({ vapiError: error })) {
         setShowPublicKeyInvalidMessage(true);
@@ -58,11 +54,26 @@ const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // call start handler
+  // Llamar al asistente usando su ID y sobrescribiendo configuraciones (opcional)
   const startCallInline = () => {
     setConnecting(true);
-    vapi.start(assistantOptions);
+
+    const assistantOverrides = {
+      transcriber: {
+        provider: "deepgram",
+        model: "nova-2",
+        language: "es",
+      },
+      recordingEnabled: false,
+      variableValues: {
+        name: "Usuario",
+      },
+    };
+
+    // Llamada al asistente utilizando el ID de Valen y sobrescribiendo configuraciones
+    vapi.start("c0a1f668-2457-455a-9145-15e11d5a3696", assistantOverrides);
   };
+
   const endCall = () => {
     vapi.stop();
   };
@@ -79,7 +90,7 @@ const App = () => {
     >
       {!connected ? (
         <Button
-          label="Call Vapi’s Pizza Front Desk"
+          label="Hablar con Valentina"
           onClick={startCallInline}
           isLoading={connecting}
         />
@@ -95,65 +106,6 @@ const App = () => {
       <ReturnToDocsLink />
     </div>
   );
-};
-
-const assistantOptions = {
-  name: "Vapi’s Pizza Front Desk",
-  firstMessage: "Vappy’s Pizzeria speaking, how can I help you?",
-  transcriber: {
-    provider: "deepgram",
-    model: "nova-2",
-    language: "en-US",
-  },
-  voice: {
-    provider: "playht",
-    voiceId: "jennifer",
-  },
-  model: {
-    provider: "openai",
-    model: "gpt-4",
-    messages: [
-      {
-        role: "system",
-        content: `You are a voice assistant for Vappy’s Pizzeria, a pizza shop located on the Internet.
-
-Your job is to take the order of customers calling in. The menu has only 3 types
-of items: pizza, sides, and drinks. There are no other types of items on the menu.
-
-1) There are 3 kinds of pizza: cheese pizza, pepperoni pizza, and vegetarian pizza
-(often called "veggie" pizza).
-2) There are 3 kinds of sides: french fries, garlic bread, and chicken wings.
-3) There are 2 kinds of drinks: soda, and water. (if a customer asks for a
-brand name like "coca cola", just let them know that we only offer "soda")
-
-Customers can only order 1 of each item. If a customer tries to order more
-than 1 item within each category, politely inform them that only 1 item per
-category may be ordered.
-
-Customers must order 1 item from at least 1 category to have a complete order.
-They can order just a pizza, or just a side, or just a drink.
-
-Be sure to introduce the menu items, don't assume that the caller knows what
-is on the menu (most appropriate at the start of the conversation).
-
-If the customer goes off-topic or off-track and talks about anything but the
-process of ordering, politely steer the conversation back to collecting their order.
-
-Once you have all the information you need pertaining to their order, you can
-end the conversation. You can say something like "Awesome, we'll have that ready
-for you in 10-20 minutes." to naturally let the customer know the order has been
-fully communicated.
-
-It is important that you collect the order in an efficient manner (succinct replies
-& direct questions). You only have 1 task here, and it is to collect the customers
-order, then end the conversation.
-
-- Be sure to be kind of funny and witty!
-- Keep all your responses short and simple. Use casual language, phrases like "Umm...", "Well...", and "I mean" are preferred.
-- This is a voice conversation, so keep your responses short, like in a real conversation. Don't ramble for too long.`,
-      },
-    ],
-  },
 };
 
 const usePublicKeyInvalid = () => {
@@ -188,7 +140,7 @@ const PleaseSetYourPublicKeyMessage = () => {
         boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
       }}
     >
-      Is your Vapi Public Key missing? (recheck your code)
+      ¿Falta tu clave pública de Vapi? (revisa tu código)
     </div>
   );
 };
@@ -210,7 +162,7 @@ const ReturnToDocsLink = () => {
         boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
       }}
     >
-      return to docs
+      Volver a la documentación
     </a>
   );
 };
